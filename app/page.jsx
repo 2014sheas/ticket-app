120d22d44aedc81d5df2a028f9f7913127d0ec73
2014sheas <54908620+2014sheas@users.noreply.github.com>
2024-01-23 22:31:19 -0500

    working with mongoDB now

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,48 @@
-import React from "react";
+import { filter } from "selenium-webdriver/lib/promise";
+import TicketCard from "./(components)/TicketCard";
 
-const Dashboard = () => {
-  return <div>Dashboard</div>;
+const getTickets = async () => {
+  try {
+    const res = await fetch("http://localhost:3000/api/Tickets", {
+      cache: "no-store",
+    });
+    return res.json();
+  } catch (error) {
+    console.log("Failed to get tciets", error);
+  }
+};
+
+const Dashboard = async () => {
+  const tickets = await getTickets();
+
+  const uniqueCategories = [
+    ...new Set(tickets?.map(({ category }) => category)),
+  ];
+
+  return (
+    <div className="p-5">
+      <div className="lg:grid grid-cols-2 xl:grid-cols-4">
+        <div>
+          {tickets &&
+            uniqueCategories?.map((uniqueCategory, categoryIndex) => (
+              <div key={categoryIndex} className="mb-4">
+                <h2>{uniqueCategory}</h2>
+                <div className="lg:grid grid-cols-2 xl:grid-cols-4"></div>
+                {tickets
+                  .filter((ticket) => ticket.category === uniqueCategory)
+                  .map((filteredTicket, _index) => (
+                    <TicketCard
+                      key={_index}
+                      id={_index}
+                      ticket={filteredTicket}
+                    />
+                  ))}
+              </div>
+            ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
