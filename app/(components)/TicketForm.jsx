481b5fa7b1687367dba44292f7fa65874af72fc3
2014sheas <54908620+2014sheas@users.noreply.github.com>
2024-01-27 14:20:39 -0500

    finished Ticket app

--- app/(components)/TicketForm.jsx
+++ app/(components)/TicketForm.jsx
@@ -3,10 +3,13 @@
 import { useRouter } from "next/navigation";
 import React, { useState } from "react";
 
-const TicketForm = () => {
+const TicketForm = ({ ticket }) => {
+  const EDITMODE = ticket._id !== "new";
   const router = useRouter();
+
   const handleChange = (e) => {
     const { name, value } = e.target;
+
     setFormData((prevState) => ({
       ...prevState,
       [name]: value,
@@ -15,14 +18,27 @@ const TicketForm = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const res = await fetch("/api/Tickets", {
-      method: "POST",
-      body: JSON.stringify({ formData }),
-      "Content-Type": "application/json",
-    });
-    if (!res.ok) {
-      throw new Error("Failed to create ticket");
+
+    if (EDITMODE) {
+      const res = await fetch(`/api/Tickets/${ticket._id}`, {
+        method: "PUT",
+        body: JSON.stringify({ formData }),
+        "Content-Type": "application/json",
+      });
+      if (!res.ok) {
+        throw new Error("Failed to update ticket");
+      }
+    } else {
+      const res = await fetch("/api/Tickets", {
+        method: "POST",
+        body: JSON.stringify({ formData }),
+        "Content-Type": "application/json",
+      });
+      if (!res.ok) {
+        throw new Error("Failed to create ticket");
+      }
     }
+
     router.refresh();
     router.push("/");
   };
@@ -36,7 +52,17 @@ const TicketForm = () => {
     category: "Hardware Problem",
   };
 
+  if (EDITMODE) {
+    startingTicketData.title = ticket.title;
+    startingTicketData.description = ticket.description;
+    startingTicketData.priority = ticket.priority;
+    startingTicketData.progress = ticket.progress;
+    startingTicketData.status = ticket.status;
+    startingTicketData.category = ticket.category;
+  }
+
   const [formData, setFormData] = useState(startingTicketData);
+
   return (
     <div className="flex justify-center">
       <form
@@ -44,7 +70,7 @@ const TicketForm = () => {
         method="post"
         onSubmit={handleSubmit}
       >
-        <h3>Create Your Ticket</h3>
+        <h3>{EDITMODE ? "Update your Ticket" : "Create Your Ticket"}</h3>
         <label> Title </label>
         <input
           id="title"
@@ -145,9 +171,11 @@ const TicketForm = () => {
           <option value="started">Started</option>
           <option value="done">Done</option>
         </select>
-        <button type="submit" className="btn max-w-xs" value="Create Ticket">
-          Submit
-        </button>
+        <input
+          type="submit"
+          className="btn"
+          value={EDITMODE ? "Update" : "Create"}
+        />
       </form>
     </div>
   );
